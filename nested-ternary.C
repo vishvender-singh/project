bd82567bdcf536936437abe8322de7e511c18ca8
vishvender-singh <vishvender.rathore2003@gmail.com>
2025-10-04 19:26:56 +0530

    Find min. Number in 4 number nested-ternary.C
    Learning nested condition

--- nested-ternary.C
+++ nested-ternary.C
@@ -7,7 +7,7 @@ void main()
  clrscr();
  printf("enter four number");
  scanf("%d%d%d%d",&a,&b,&c,&d);
- ans=(a<b && a<c && a<d)? a : (b<c && b<a && b<d)? b : (c<a && c<b && c<d) ? c:d;
+ ans=(a<b && a<c && a<d)? a : (b<c && b<d)? b : (c<d) ? c:d;
  printf(" min no is %d",ans);
  getch();
- }
+}
